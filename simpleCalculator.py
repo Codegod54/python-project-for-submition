2e0963a29435bdea0326b29f7b4eda51022d4c31
basant chaudhary <bmagar932@mail.com>
2025-02-18 21:51:18 +0545

    revised code

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,85 +1,99 @@
-# save the result until the user doesn't enter 6
-# display the results when the user enters 7
-# exit when the user enters 5
-# once the operation is done, ask the user to enter the next operation
-# the user can perform multiple operations
-# the user can save multiple results
-# the user can display all the saved results
-# save the operator and the result in the database
-
+from datetime import datetime
 import sqlite3
+import math
 
-def save_result(operator, result):
+def date_time():
+    now = datetime.now()
+    dt = now.date()
+    ti = now.time()
+    return dt, ti
+
+def save_result(numa, operator, numb, result, date, time):
     conn = sqlite3.connect('calculator.db')
     cursor = conn.cursor()
 
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS results (
             id INTEGER PRIMARY KEY,
+            numa REAL,
             operator TEXT,
-            result INTEGER
+            numb REAL,
+            result REAL,
+            date str,
+            time str
         )
+
     ''')
 
     cursor.execute('''
-        INSERT INTO results (operator, result) VALUES (?, ?)
-    ''', (operator, result))
+        INSERT INTO results (numa, operator, numb, result, date, time) VALUES (?, ?, ?, ?, ?, ?)
+    ''', (numa, operator, numb, result, date, time ))
 
     conn.commit()
     conn.close()
 
-def display_results():
+
+def display_history():
     conn = sqlite3.connect('calculator.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM results')
     results = cursor.fetchall()
     for result in results:
-        print(f"{result[1]}: {result[2]}")
+        print(f"{result[1]} {result[2]} {result[3]} = {result[4]} on {result[5]} at {result[6]}")
     conn.close()
 
 def main():
     while True:
-        g = int(input("""Select the function:
-            1. Addition
-            2. Subtraction
-            3. Multiplication
-            4. Division
-            5. Exit
-            6. Display results
-                      """))
+
+        try:
+            g = int(input("""Select the function:
+                1. Addition
+                2. Subtraction
+                3. Multiplication
+                4. Division
+                5. Exit
+                6. Display history
+                        """))
+        except:
+            print("Invalid input. Please enter a valid number.")
+            continue
 
         if g == 5:
             break
 
         if g == 6:
-            display_results()
+            display_history()
             continue
-
-        a = int(input("Enter the first number: "))
-        b = int(input("Enter the second number: "))
-
+        try:
+    
+            a = float(input("Enter the first number: "))
+            b = float(input("Enter the second number: "))
+        except:
+            print("character not accepted")
+            continue
         if g == 1:
-            result = a + b
+            result = math.fsum(a, b)
             operator = "Addition"
         elif g == 2:
-            result = a - b
+            result = math.fsum(a, -b)
             operator = "Subtraction"
         elif g == 3:
             result = a * b
             operator = "Multiplication"
         elif g == 4:
+            if b == 0:
+                print("Error: Division by zero.")
+                continue
             result = a / b
             operator = "Division"
+
         else:
             print("Invalid input")
             continue
 
         print(f"The result is: {result}")
-        h = input("Do you want to save the result? Enter 'y' to save the result: ")
-        if h == 'y':
-            save_result(operator, result)
-        else:
-            print("The result is not saved.")
+        dt, ti = date_time()
+        save_result(a, operator, b, result, str(dt), str(ti))
 
 if __name__ == "__main__":
     main()
